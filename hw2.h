86d5cb9a07adc8ac518d9c7215823ef6dd8580cc
lens <117731232+cclens@users.noreply.github.com>
2023-11-14 23:33:22 +0700

    Update hw2.h

--- hw2.h
+++ hw2.h
@@ -5,8 +5,13 @@ int main()
     int N = 0;
     scanf("%d", &N);
     int arr[N];
-    for (int i = N; i >=1; i--){
+    for (int i = 1; i <= N; i++){
         arr[i] = i;
         printf("%d", arr[i]);
+    }
+    printf("\n");
+    for (int j =0; j < N; j++){
+        arr[j] = N -j;
+        printf("%d", arr[j]);
     }
 }

c202ea3e176d11a4d3886fe2b1da41f7364ea8c6
lens <117731232+cclens@users.noreply.github.com>
2024-02-21 12:07:35 +0700

    Delete sub.c

--- sub.c
+++
@@ -1,8 +0,0 @@
-float sub(float* buffer){
-    float temp = 0;
-    printf("%s\n", "введите число которое хотите отнять");
-    scanf("%f", &temp);
-    *buffer -= temp;
-    printf("%f\n", *buffer);
-    temp = 0;
-}

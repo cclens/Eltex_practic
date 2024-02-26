72e1a685cac6125a52cf56542624d32b72a1db98
lens <117731232+cclens@users.noreply.github.com>
2024-02-27 00:32:14 +0700

    Update add.c

--- Dynamic_calculator/add.c
+++ Dynamic_calculator/add.c
@@ -3,7 +3,7 @@
 void add(float** buffer){
       float* tmp = (float*)malloc(sizeof(float));
       printf("%s\n", "введите число которое хотите прибавить");
-      scanf("%f", tmp);
+      scanf("%10f", tmp);
       if (tmp == NULL){
           printf("ошибка выделения памяти\n");
           return;

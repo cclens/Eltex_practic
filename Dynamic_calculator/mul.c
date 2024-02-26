75f761256a79d8f9063d70997397d64c38e524f1
lens <117731232+cclens@users.noreply.github.com>
2024-02-27 00:32:51 +0700

    Update mul.c

--- Dynamic_calculator/mul.c
+++ Dynamic_calculator/mul.c
@@ -3,7 +3,7 @@
 void mul (float** buffer){
       float* tmp = (float*)malloc(sizeof(float));
       printf("%s\n", "введите число на которое хотите умножить");
-      scanf("%f", tmp);
+      scanf("%10f", tmp);
       if (tmp == NULL){
           printf("ошибка выделения памяти\n");
           return;

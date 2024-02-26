c250dd06fe112f51debf819870819fdd994a9f62
lens <117731232+cclens@users.noreply.github.com>
2024-02-27 00:30:47 +0700

    Update div.c

--- div.c
+++ div.c
@@ -1,7 +1,7 @@
 float division(float* buffer){
     float temp = 0;
     printf("введите число на которое хотите разделить\n");
-    scanf("%f", &temp);
+    scanf("%10f", &temp);
     if (temp == 0){
         printf("на ноль нельзя\n");
         exit(1);

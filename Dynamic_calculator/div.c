d6fd2b4f46fbb111d3112f278bb311cff02edcb5
lens <117731232+cclens@users.noreply.github.com>
2024-02-27 00:32:22 +0700

    Update div.c

--- Dynamic_calculator/div.c
+++ Dynamic_calculator/div.c
@@ -3,7 +3,7 @@
 void division(float** buffer){
       float* tmp = (float*)malloc(sizeof(float));
       printf("%s\n", "введите число на которое хотите разделить");
-      scanf("%f", tmp);
+      scanf("%10f", tmp);
       if (tmp == NULL){
           printf("ошибка выделения памяти\n");
           return;

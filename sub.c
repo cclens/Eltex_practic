0acd91e7e08bb65df54abb8501fdbd889f93ea81
lens <117731232+cclens@users.noreply.github.com>
2024-02-27 00:31:26 +0700

    Update sub.c

--- sub.c
+++ sub.c
@@ -1,7 +1,7 @@
 float sub(float* buffer){
     float temp = 0;
     printf("%s\n", "введите число которое хотите отнять");
-    scanf("%f", &temp);
+    scanf("%10f", &temp);
     *buffer -= temp;
     printf("%f\n", *buffer);
     temp = 0;

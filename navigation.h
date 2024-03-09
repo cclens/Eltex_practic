6e7993ee2ee5cbcb8b1dc7515c6cdc2a66a724c4
cclens <ivankudyshevvv@gmail.com>
2024-03-09 04:30:21 -0500

    uptdate .h files

---
+++ navigation.h
@@ -0,0 +1,9 @@
+#ifndef NAVIGATION_H
+#define NAVIGATION_H
+
+
+void current_directory(int *count, char **filenames, int *index);
+void enter_directory(char **filenames, int index);
+
+
+#endif /* end NAVIGATION.G */ 

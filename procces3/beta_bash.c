89e1c4f0c1df0c2fc0d9d2319321de1a6d883480
lens <117731232+cclens@users.noreply.github.com>
2024-06-25 13:50:16 +0700

    Update beta_bash.c

--- procces3/beta_bash.c
+++ procces3/beta_bash.c
@@ -5,29 +5,43 @@
 #include <string.h>
 
 int main(){
-	
-	char command[15];
-	
-	while(strncmp(command, "exit", 4) != 0){
-		printf("введите команду которую хотите исполнить, для выхода введите exit \n");
-		fgets(command, sizeof(command), stdin);
-		command[strcspn(command, "\n")] = 0;
-		pid_t main_pid = fork();
-		
-		if (main_pid < 0){
-			printf("ошибка вызова процесса main_pid\n");
-			exit(EXIT_FAILURE);
-		}
-		else if (main_pid == 0){
-			if (system(command) == (-1)){
-				printf("ошибка выполнения команды :) \n");
-			}
-		 	exit(EXIT_SUCCESS);
-		}
-		 else {
-            int status = 0; 
-            waitpid(main_pid, &status, 0); // Ожидание завершения дочернего процесса
-        }
-	}
+  
+  char command[15];
+  char *args[10];
+  
+  while(1){
+    printf("введите команду которую хотите исполнить, для выхода введите exit \n");
+    fgets(command, sizeof(command), stdin);
+    command[strcspn(command, "\n")] = 0;
 
+    if (strncmp(command, "exit", 4) == 0) {
+      break;
+    }
+
+    
+    int i = 0;
+    args[i] = strtok(command, " ");
+    while(args[i] != NULL) {
+      args[++i] = strtok(NULL, " ");
+    }
+
+    pid_t main_pid = fork();
+    
+    if (main_pid < 0){
+      printf("ошибка вызова процесса main_pid\n");
+      exit(EXIT_FAILURE);
+    }
+    else if (main_pid == 0){
+      if (execvp(args[0], args) == -1){
+        printf("ошибка выполнения команды :) \n");
+      }
+       exit(EXIT_FAILURE);
+    }
+    else {
+      int status = 0; 
+      waitpid(main_pid, &status, 0); 
+    }
+  }
+
+  return 0;
 }

caf51f2361bd915d49a36ff519cfdee45e784a71
lens <117731232+cclens@users.noreply.github.com>
2024-06-27 11:39:39 +0700

    Update Server.c

--- msg_que/POSIX/Server.c
+++ msg_que/POSIX/Server.c
@@ -15,20 +15,17 @@ int main() {
     char buffer[MAX_MSG_SIZE + 1]; // Увеличиваем размер буфера на 1 для '\0'
     int msg_len;
 
-    // Открываем очередь сообщений
     mq = mq_open(QUEUE_NAME, O_CREAT | O_RDWR, 0666, NULL);
     if (mq == (mqd_t)-1) {
         perror("mq_open");
         exit(1);
     }
 
-    // Получаем атрибуты очереди, чтобы узнать максимальный размер сообщения
     if (mq_getattr(mq, &attr) == -1) {
         perror("mq_getattr");
         exit(1);
     }
 
-    // Читаем сообщение от клиента
     msg_len = mq_receive(mq, buffer, attr.mq_msgsize, NULL);
     if (msg_len == -1) {
         perror("mq_receive");
@@ -38,20 +35,17 @@ int main() {
     buffer[msg_len] = '\0';
     printf("Сервер получил сообщение: %s\n", buffer);
 
-    // Отправляем ответ клиенту
     sprintf(buffer, "Hello!");
     if (mq_send(mq, buffer, strlen(buffer) + 1, 0) == -1) {
         perror("mq_send");
         exit(1);
     }
 
-    // Закрываем очередь
     if (mq_close(mq) == -1) {
         perror("mq_close");
         exit(1);
     }
 
-    // Удаляем очередь
     if (mq_unlink(QUEUE_NAME) == -1) {
         perror("mq_unlink");
         exit(1);

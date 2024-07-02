243e1ce98f5b7013b9c701946ee5abdbbdb25f6a
lens <117731232+cclens@users.noreply.github.com>
2024-07-03 00:36:46 +0700

    Update Server.c

--- chat/Server.c
+++ chat/Server.c
@@ -1,64 +1,96 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/ipc.h>
-#include <sys/msg.h>
-#include <sys/types.h>
 #include <unistd.h>
+#include <string.h>
+#include <mqueue.h>
+#include <pthread.h>
 
-#define MAX_TEXT 512
-#define SERVER_QUEUE_KEY 1234
+struct mq_attr attr;
+mqd_t mq_in;
+mqd_t mq_out;
+mqd_t mq_adm_in;
+mqd_t mq_adm_out;
+mqd_t mq_list_in;
+mqd_t mq_list_out;
 
-struct message {
-    long msg_type;
-    char text[MAX_TEXT];
-};
+pthread_t thread_main;
+pthread_t thread_count;
+pthread_t thread_list;
 
-int main() {
-    int server_msgid;
-    struct message msg;
-    key_t key = SERVER_QUEUE_KEY;
-    char users[10][50];
-    int num_users = 0;
 
-    server_msgid = msgget(key, 0666 | IPC_CREAT);
-    if (server_msgid == -1) {
-        perror("msgget failed");
-        exit(EXIT_FAILURE);
-    }
+char buffer[100];
+int count = 0;
+char name_list[100][100];
 
-    printf("Server started. Server message queue ID: %d\n", server_msgid);
-
-    while (1) {
-        if (msgrcv(server_msgid, (void *)&msg, sizeof(msg.text), 0, 0) == -1) {
-            perror("msgrcv failed");
-            continue;
+/*
+void Track_user(){
+        for (int i = 0; i <= count; i++){
+        for (int j = 0; j <= count; j++){
+                printf("%s\n", name_list[i]);
+                mq_send(mq_list_in, name_list[i], strlen(name_list[i])+1, 0);
         }
-
-        if (msg.msg_type == 2) {
-            // Сообщение о подключении нового пользователя
-            strcpy(users[num_users], msg.text);
-            num_users++;
-            for (int i = 0; i < num_users; ++i) {
-                struct message user_list_msg;
-                user_list_msg.msg_type = 2;
-                strcpy(user_list_msg.text, users[i]);
-                if (msgsnd(server_msgid, (void *)&user_list_msg, sizeof(user_list_msg.text), 0) == -1) {
-                    perror("msgsnd failed");
-                }
-            }
-        } else {
-            printf("Received message: %s\n", msg.text);
-            for (int i = 0; i < num_users; ++i) {
-                if (msgsnd(server_msgid, (void *)&msg, sizeof(msg.text), 0) == -1) {
-                    perror("msgsnd failed");
-                }
-            }
         }
+}
+*/
+void* User_count(){
+    while(1){
+                mq_receive(mq_adm_in, buffer, sizeof(buffer), NULL);
+                        strcpy(name_list[count], buffer);
+        //              printf("%d\n", count);
+
+                        for (int i = 0; i < count; i++){
+                mq_send(mq_adm_out, buffer, strlen(buffer)+1, 0);
+                        } 
+
+            for (int i = 0; i <= count; i++){
+                    for (int j = 0; j <= count; j++){
+                            printf("%s\n", name_list[i]);
+                            mq_send(mq_list_in, name_list[i], strlen(name_list[i])+1, 0);
+                    }
+            }
+
+                        if (strcmp(buffer, "exit\0") == 0){
+                        count -= 1;
+                        printf("%d\n", count);
+                }
+            else{
+                count += 1;
+            }
     }
-
-    msgctl(server_msgid, IPC_RMID, NULL);
-
-    return 0;
 }
 
+void* Join_track(){
+    while (1){
+        mq_receive(mq_in, buffer, sizeof(buffer), NULL);
+        printf("Received message from client: %s\n", buffer);
+        for (int i = 0; i < count; i++){
+            mq_send(mq_out, buffer, strlen(buffer)+1, 0);
+        }
+    }
+}
+ 
+int main(){
+    attr.mq_msgsize = 100;
+    attr.mq_curmsgs = 0;
+    attr.mq_flags; 
+    attr.mq_maxmsg = 10;
+
+    mq_in = mq_open("/flag.txt1", O_CREAT|O_RDWR, 0666, &attr);
+    mq_out = mq_open("/flag.txt2", O_CREAT|O_RDWR, 0666, &attr);
+    mq_adm_in = mq_open("/flag.txt3", O_CREAT|O_RDWR, 0666, &attr);
+    mq_adm_out = mq_open("/flag.txt4", O_CREAT|O_RDWR, 0666, &attr);
+        mq_list_in = mq_open("/flag.txt5", O_CREAT|O_RDWR, 0666, &attr);
+
+    pthread_create(&thread_main, NULL, Join_track, NULL);
+    pthread_create(&thread_count, NULL, User_count, NULL);
+
+
+    pthread_join(thread_main, NULL);
+    pthread_join(thread_count, NULL);
+
+    mq_close(mq_in);
+    mq_close(mq_out);
+    mq_close(mq_adm_in);
+    mq_close(mq_adm_out);
+        mq_close(mq_list_in);
+        mq_close(mq_list_out);
+}
